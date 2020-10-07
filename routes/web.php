9070a4a62c771fb536b1e92ad15c6886176886d3
Supavit Soontorn <fivexoxo@hotmail.com>
2020-10-07 18:06:31 +0700

    add create user.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('/', 'App\Http\Controllers\DashboardController@index');
+
+//ADMIN
+Route::get('/member/create', 'App\Http\Controllers\Admin\UserController@index');
