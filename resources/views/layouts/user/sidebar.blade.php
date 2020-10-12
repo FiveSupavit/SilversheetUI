42708f7c837ce0c30f273a9a95925f8a65951a29
Supavit Soontorn <fivexoxo@hotmail.com>
2020-10-12 14:05:04 +0700

    fix lowercase name for user in sidebar.

--- resources/views/layouts/user/sidebar.blade.php
+++ resources/views/layouts/user/sidebar.blade.php
@@ -6,24 +6,24 @@
                     <li class="site-menu-item has-sub">
                         <a href="{{ url('user/dashboard') }}">
                             <i class="site-menu-icon wb-dashboard" aria-hidden="true"></i>
-                            <span class="site-menu-title">DASHBOARD</span>
+                            <span class="site-menu-title">Dashboard</span>
                         </a>
                     </li>
                     <li class="site-menu-item has-sub">
                         <a href="javascript:void(0)">
                             <i class="site-menu-icon wb-layout" aria-hidden="true"></i>
-                            <span class="site-menu-title">CERTIFICATE</span>
+                            <span class="site-menu-title">Certificate</span>
                             <span class="site-menu-arrow"></span>
                         </a>
                         <ul class="site-menu-sub">
                             <li class="site-menu-item">
                                 <a class="animsition-link" href="{{ url('user/verified_certificate') }}">
-                                    <span class="site-menu-title">VERIFIED CERTIFICATE</span>
+                                    <span class="site-menu-title">Verified Certificate</span>
                                 </a>
                             </li>
                             <li class="site-menu-item">
                                 <a class="animsition-link" href="{{ url('user/certificate') }}">
-                                    <span class="site-menu-title">NEED INPUT</span>
+                                    <span class="site-menu-title">Need Input</span>
                                     <div class="site-menu-label">
                                         <span class="badge badge-danger badge-round mr-25">6</span>
                                     </div>
@@ -34,31 +34,31 @@
                     <li class="site-menu-item has-sub">
                         <a href="javascript:void(0)">
                             <i class="site-menu-icon wb-file" aria-hidden="true"></i>
-                            <span class="site-menu-title">REPORTS</span>
+                            <span class="site-menu-title">Reports</span>
                             <span class="site-menu-arrow"></span>
                         </a>
                         <ul class="site-menu-sub">
                             <li class="site-menu-item has-sub">
                                 <a href="javascript:void(0)">
-                                    <span class="site-menu-title">TYPE1</span>
+                                    <span class="site-menu-title">Type1</span>
                                     <span class="site-menu-arrow"></span>
                                 </a>
                                 <ul class="site-menu-sub">
                                     <li class="site-menu-item">
                                         <a class="animsition-link" href="pages/error-400.html">
-                                            <span class="site-menu-title">TYPE1_1</span>
+                                            <span class="site-menu-title">Type1_1</span>
                                         </a>
                                     </li>
                                     <li class="site-menu-item">
                                         <a class="animsition-link" href="pages/error-403.html">
-                                            <span class="site-menu-title">TYPE1_2</span>
+                                            <span class="site-menu-title">Type1_2</span>
                                         </a>
                                     </li>
                                 </ul>
                             </li>
                             <li class="site-menu-item">
                                 <a class="animsition-link" href="pages/faq.html">
-                                    <span class="site-menu-title">TYPE2</span>
+                                    <span class="site-menu-title">Type2</span>
                                 </a>
                             </li>
                         </ul>
